3de2db3c3cde9809f4a1b9dfabeda27b342aa6dd
zong0jie <zong0jie@06180768-fef1-4543-b677-0f5c5c0ac3a4>
2011-05-04 13:33:59 +0000

    git-svn-id: http://127.0.0.1/zjprivate/eclipse/NBCplatform/trunk@45 06180768-fef1-4543-b677-0f5c5c0ac3a4

--- analysis/src/java/com/novelBio/annotation/copeID/ItemInfo.java
+++
@@ -1,9 +0,0 @@
-package com.novelBio.annotation.copeID;
-/**
- * 专门用与FisherTest类中获得Item的情况
- * @author zong0jie
- *
- */
-public interface ItemInfo {
-	public String[] getItemName(String ItemID);
-}
