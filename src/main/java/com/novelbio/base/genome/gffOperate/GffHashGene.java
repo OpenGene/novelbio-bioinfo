2ea150296b0169210061af0b1df86284ab0f61ea
zong0jie <zong0jie@06180768-fef1-4543-b677-0f5c5c0ac3a4>
2011-06-22 07:54:17 +0000

    git-svn-id: http://127.0.0.1/zjprivate/eclipse/NBCplatform/trunk@103 06180768-fef1-4543-b677-0f5c5c0ac3a4

--- src/main/java/com/novelbio/base/genome/gffOperate/GffHashGene.java
+++
@@ -1,9 +0,0 @@
-package com.novelbio.base.genome.gffOperate;
-
-import java.util.ArrayList;
-import java.util.Hashtable;
-
-public abstract class GffHashGene extends GffHash
-{
-	public abstract ArrayList<Long> getGeneStructureLength();
-}
