f8bc45a42349b778968db69cb05e067cde6744ee
Zong Jie <zong0jie@gmail.com>
2012-12-31 01:50:28 +0800

    1. 想把bam文件的sort集成到平台实现

--- src/main/java/com/novelbio/analysis/seq/sam/BamSort.java
+++ src/main/java/com/novelbio/analysis/seq/sam/BamSort.java
@@ -1,11 +1,20 @@
 package com.novelbio.analysis.seq.sam;
 
+import java.io.File;
+
+import net.sf.samtools.SAMFileHeader.SortOrder;
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+
 import com.novelbio.base.cmd.CmdOperate;
 import com.novelbio.base.fileOperate.FileOperate;
 
 public class BamSort {
 //	samtools view -bt /media/winE/Bioinformatics/GenomeData/human/ucsc_hg19/Index/bwa_chromFa/UCSC_hg19.fa.fai $SAMFile  > "$SAMPrix".bam
 	
+	private static final String INPUT = null;
 	String ExePath = "";
 	String bamFile;
 	/**
@@ -27,4 +36,28 @@ public class BamSort {
 		cmdOperate.run();
 		return FileOperate.changeFileSuffix(outFile, "", "") + ".bam";
 	}
+	
+	   
+	public SAMFileHeader.SortOrder SORT_ORDER;
+	
+	private void test(String sortBamFile) {
+		File fileOut = new File(sortBamFile);
+		SamFile samFile = new SamFile("");
+		SAMFileReader reader = samFile.samReader.samFileReader;
+        reader.getFileHeader().setSortOrder(SORT_ORDER);
+        SAMFileWriter writer = new SAMFileWriterFactory().makeSAMOrBAMWriter(reader.getFileHeader(), false, fileOut);
+
+        ProgressLogger progress = new ProgressLogger(log, (int) 1e7, "Read");
+        for (final SAMRecord rec: reader) {
+            writer.addAlignment(rec);
+            progress.record(rec);
+        }
+
+        log.info("Finished reading inputs, merging and writing to output now.");
+
+        reader.close();
+        writer.close();
+        return 0;
+	}
+	
 }
