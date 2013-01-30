5dbabdce4359d8b5788819f27d73ca1da6752c20
zong0jie <zong0jie@gmail.com>
2013-01-30 10:33:58 +0800

    1. 删除base包，这个包现在开始从外部引用

--- src/main/java/com/novelbio/base/multithread/txtreadcopewrite/MTRecordCope.java
+++
@@ -1,5 +0,0 @@
-package com.novelbio.base.multithread.txtreadcopewrite;
-/** 处理好的每条记录，包装到这里 */
-public interface MTRecordCope {
-
-}
--- src/main/java/com/novelbio/base/plot/heatmap/ColorInflect.java
+++
@@ -1,9 +0,0 @@
-package com.novelbio.base.plot.heatmap;
-/**
- * 颜色和拐点的类
- * @author zong0jie
- *
- */
-public class ColorInflect {
-
-}
