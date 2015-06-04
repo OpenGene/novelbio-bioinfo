dcd8317b881b8e234aca0224779f20c6aef3b77b
bianlianle <bianlianle@novelbio.com>
2015-06-04 14:31:29 +0800

    1.添加注释

--- src/main/java/com/novelbio/analysis/seq/resequencing/EnumVariantClass.java
+++ src/main/java/com/novelbio/analysis/seq/resequencing/EnumVariantClass.java
@@ -17,13 +17,13 @@ public enum EnumVariantClass {
 	Missense_Mutation,
 	/**无义突变*/
 	Nonsense_Mutation,
-	/***/
+	/**沉默突变：是一种DNA突变，不显著改变生物表型，此类突变可以发生在non-coding 区域，也可以发生在exon区域，当发生在exon区域时，不引起氨基酸的改变 */
 	Silent, 
 	/** 间接位点突变 */
 	Splice_Site,
 	/** 转录起始位点突变 */
 	Translation_Start_Site,
-	/** 终止密码子突变 */
+	/** 终止密码子突变后，变为非终止密码子 */
 	Nonstop_Mutation,
 	UTR3,
 	/** 3'侧翼突变 */
@@ -34,6 +34,7 @@ public enum EnumVariantClass {
 	/** 基因间区突变 */
 	IGR1,
 	Intron,
+	/** non-coding transcript 变异 */
 	RNA,
 	/** 靶区域突变 */
 	Targeted_Region
