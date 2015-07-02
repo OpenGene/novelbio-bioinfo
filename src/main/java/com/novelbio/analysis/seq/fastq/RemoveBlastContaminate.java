6f7a5392934b44047c25526510dad9cf4635e637
Zong Jie <zongjie@novelbio.com>
2015-07-02 09:54:00 +0800

    1. 写了个根据blast结果过滤fastq的类，并写了双端情况下的测试

---
+++ src/main/java/com/novelbio/analysis/seq/fastq/RemoveBlastContaminate.java
@@ -0,0 +1,139 @@
+package com.novelbio.analysis.seq.fastq;
+
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+
+import com.novelbio.analysis.seq.fastq.FastQ;
+import com.novelbio.analysis.seq.fastq.FastQRecord;
+import com.novelbio.base.dataOperate.TxtReadandWrite;
+import com.novelbio.base.fileOperate.FileOperate;
+
+/** 
+ * 给定fastq文件，和blast结果，删除比对到其他物种的序列
+ * @author novelbio
+ *
+ */
+public class RemoveBlastContaminate {
+	private static final Logger logger = Logger.getLogger(RemoveBlastContaminate.class);
+	
+	String blastFile;
+	
+	Iterator<FastQRecord[]> itFqPE;
+	Iterator<FastQRecord> itFqSE;
+	
+	FastQ fqLeft, fqRight;
+	FastQ fqLeftModify, fqRightModify;
+	
+	boolean isTest = false;
+	
+	public static void main(String[] args) {
+		TxtReadandWrite txtRead = new TxtReadandWrite("/home/novelbio/下载/blastn.txt");
+		TxtReadandWrite txtWrite = new TxtReadandWrite("/home/novelbio/下载/blastn_modify.txt", true);
+		int i = 0;
+		for (String string : txtRead.readlines()) {
+			if (i++ > 10000) {
+				break;
+			}
+			txtWrite.writefileln(string);
+		}
+		txtRead.close();
+		txtWrite.close();
+	}
+	
+	public void setFastqLeft(String fastqLeft) {
+		this.fqLeft = new FastQ(fastqLeft);
+		fqLeftModify = new FastQ(FileOperate.changeFileSuffix(fastqLeft, "_remove", "fq.gz|fq|fastq|fastq.gz", null), true);
+	}
+	
+	/** 单端测序不用设置这个 */
+	public void setFastqRight(String fastqRight) {
+		this.fqRight = new FastQ(fastqRight);
+		fqRightModify = new FastQ(FileOperate.changeFileSuffix(fastqRight, "_remove", "fq.gz|fq|fastq|fastq.gz", null), true);
+	}
+	public void setBlastFile(String blastFile) {
+		this.blastFile = blastFile;
+	}
+	/** 是否将结果写入fastq，主要用在测试环境，如仅测 {@link #getFqExistNameAndWriteSeqNotExistInBlast(String)}
+	 * 默认为false
+	 *  */
+	protected void setIsWriteFastq(boolean isTest) {
+		this.isTest = isTest;
+	}
+	public void initial() {
+		if (fqRight != null) {
+			itFqPE =  fqLeft.readlinesPE(fqRight).iterator();
+		} else {
+			itFqSE = fqLeft.readlines().iterator();
+		}
+	}
+	
+	public void runRemove() {
+		TxtReadandWrite txtReadBlast = new TxtReadandWrite(blastFile);		
+		
+		String fqNameLast = "";
+		for (String blastInfo : txtReadBlast.readlines()) {
+			String fqNameToDelete = blastInfo.split("\t")[0];
+			if (fqNameToDelete.equals(fqNameLast)) {
+				continue;
+			}
+			getFqExistNameAndWriteSeqNotExistInBlast(fqNameToDelete);
+			fqNameLast = fqNameToDelete;
+		}
+		while (itFqPE.hasNext()) {
+			FastQRecord[] pe = itFqPE.next();
+			writeFastq(pe);
+		}
+		
+		fqLeftModify.close();
+		if (fqRightModify != null) {
+			fqRightModify.close();
+		}
+		
+		txtReadBlast.close();
+	
+	}
+	
+	protected String getFqExistNameAndWriteSeqNotExistInBlast(String fqNameToDelete) {
+		String seqNameExist = null;
+		while (true) {
+			String fqName = null;
+			FastQRecord[] fqPE = null;
+			if (itFqPE != null) {
+				fqPE = itFqPE.next();
+				fqName = fqPE[0].getName().split(" ")[0];
+			} else {
+				FastQRecord fqSE = itFqSE.next();
+				fqPE = new FastQRecord[]{fqSE, null};
+				fqName = fqSE.getName().split(" ")[0];
+			}
+		
+			
+			if (fqName.equals(fqNameToDelete)) {
+				seqNameExist = fqName;
+				break;
+			} else {
+				if (!isTest) {
+					writeFastq(fqPE);
+				}
+			}
+		}
+		return seqNameExist;
+	}
+	
+	protected void writeFastq(FastQRecord[] fqPE) {
+		fqLeftModify.writeFastQRecord(fqPE[0]);
+		if (fqPE[1] != null) {
+			fqRightModify.writeFastQRecord(fqPE[1]);
+		}
+	}
+	
+	/** remove结束后才可使用 */
+	public FastQ getResultFqLeft() {
+		return fqLeftModify;
+	}
+	/** remove结束后才可使用 */
+	public FastQ getResultFqRight() {
+		return fqRightModify;
+	}
+}
---
+++ src/test/java/com/novelbio/analysis/seq/fastq/TestRemoveBlastContaminate.java
@@ -0,0 +1,70 @@
+package com.novelbio.analysis.seq.fastq;
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import com.novelbio.analysis.seq.fastq.FastQ;
+import com.novelbio.analysis.seq.fastq.FastQRecord;
+import com.novelbio.base.dataOperate.TxtReadandWrite;
+
+
+public class TestRemoveBlastContaminate extends TestCase {
+	String parentPath = "/home/novelbio/下载/";
+	String blastTest = parentPath + "blastn_modify.txt";
+	String fastqL = parentPath + "Test1.fq";
+	String fastqR = parentPath + "Test2.fq";
+	
+	public void testGetFqExistNameAndWriteSeqNotExistInBlast() {
+		RemoveBlastContaminate remove = new RemoveBlastContaminate();
+		remove.setIsWriteFastq(true);
+		remove.setBlastFile(blastTest);
+		remove.setFastqLeft(fastqL);
+		remove.setFastqRight(fastqR);
+		
+		remove.initial();
+		
+		String seq1 = "HWI-D00552:106:C6DDEANXX:1:1101:1700:2243";
+		String seq2 = "HWI-D00552:106:C6DDEANXX:1:1101:1951:2167";
+		String seq3 = "HWI-D00552:106:C6DDEANXX:1:1101:1753:2173";
+		assertEquals(seq1, remove.getFqExistNameAndWriteSeqNotExistInBlast(seq1));
+		assertEquals(seq2, remove.getFqExistNameAndWriteSeqNotExistInBlast(seq2));
+		assertEquals(seq3, remove.getFqExistNameAndWriteSeqNotExistInBlast(seq3));
+	}
+	
+	/** 测试总体功能 */
+	public void testFunction() {
+		RemoveBlastContaminate remove = new RemoveBlastContaminate();
+		remove.setBlastFile(blastTest);
+		remove.setFastqLeft(fastqL);
+		remove.setFastqRight(fastqR);
+		remove.initial();
+		remove.runRemove();
+		
+		TxtReadandWrite txtBlast = new TxtReadandWrite(blastTest);
+		Set<String> setBlast = new HashSet<>();
+		for (String blast : txtBlast.readlines()) {
+			setBlast.add(blast.split("\t")[0]);
+		}
+		txtBlast.close();
+		FastQ fastQL = new FastQ(fastqL);
+		
+		int fqRecordNum = 0;
+		for (FastQRecord fastQRecord : fastQL.readlines()) {
+			if (setBlast.contains(fastQRecord.getName().split(" ")[0])) {
+				continue;
+			}
+			fqRecordNum++;
+		}
+		fastQL.close();
+		
+		int realNum = 0;
+		FastQ fastqReal = new FastQ(remove.getResultFqLeft().getReadFileName());
+		for (FastQRecord fq : fastqReal.readlines()) {
+			realNum++;
+		}
+		fastqReal.close();
+		assertEquals(fqRecordNum, realNum);		
+	}
+	
+}
