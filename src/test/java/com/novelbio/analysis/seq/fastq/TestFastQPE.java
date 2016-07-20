9e7a548d50da23600c2975100180fdf4cbac86a1
zongjie <zongjie@novelbio.com>
2016-07-20 22:28:38 +0800

    1. 修正Blast建索引 2. 修正FastQC过滤双端序列时可以跳过一系列reads

--- src/test/java/com/novelbio/analysis/seq/fastq/TestFastQPE.java
+++ src/test/java/com/novelbio/analysis/seq/fastq/TestFastQPE.java
@@ -7,8 +7,24 @@ import org.junit.Test;
 public class TestFastQPE {
 	@Test
 	public void testReadPairend() {
-		String file1 = "src/test/resources/test_file/fastq/PE/Lerror.fq";
-		String file2 = "src/test/resources/test_file/fastq/PE/Rerror.fq";
+		String file1 = "src/test/resources/test_file/fastq/PE/L_correct.1.fq";
+		String file2 = "src/test/resources/test_file/fastq/PE/R_correct.2.fq";
+		FastQ fastQ = new FastQ(file1);
+		FastQ fastQ2 = new FastQ(file2);
+		int i = 0;
+		for (FastQRecord[] pe : fastQ.readlinesPE(fastQ2)) {
+			i++;
+			Assert.assertEquals(true, FastQRecord.isPairedByName(pe[0], pe[1]));
+		}
+		fastQ.close();
+		fastQ2.close();
+		Assert.assertEquals(1924, i);
+	}
+	
+	@Test
+	public void testReadPairend2() {
+		String file1 = "src/test/resources/test_file/fastq/PE/Lerror.fq.gz";
+		String file2 = "src/test/resources/test_file/fastq/PE/Rerror.fq.gz";
 		FastQ fastQ = new FastQ(file1);
 		FastQ fastQ2 = new FastQ(file2);
 		int i = 0;
