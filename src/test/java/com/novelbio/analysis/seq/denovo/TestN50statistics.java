dc94c7ecaa2952ea38a0c5fe8bcff18ac1cb4870
bianlianle <bianlianle@novelbio.com>
2016-09-14 17:59:06 +0800

    1. 修改getN50Len的测试代码

--- src/test/java/com/novelbio/analysis/seq/denovo/TestN50statistics.java
+++ src/test/java/com/novelbio/analysis/seq/denovo/TestN50statistics.java
@@ -12,20 +12,22 @@ import com.novelbio.analysis.ExceptionNBCsoft;
 /** 测试N50的统计 */
 public class TestN50statistics {
 	@Test
-	public void testN50() {		
-//		String chrFile = "src/test/resources/test_file/denovo/merged.cap3.singlets.fa";
-		String chrFile = "/home/novelbio/git/NBCplatform/src/test/resources/test_file/denovo/Trinity.fasta.gz";
+	public void testN50() {	
+		
+		String chrFile = "src/test/resources/test_file/denovo/merged.cap3.singlets.fa";	
 		N50statistics n50 = new N50statistics(chrFile);
 		n50.doStatistics();
 		n50.getLsNinfo();
-//		Assert.assertEquals(1705, n50.getAllContigsLen());
-//		Assert.assertEquals(266, n50.getN50Len());
+		Assert.assertEquals(1705, n50.getAllContigsLen());
+		Assert.assertEquals(266, n50.getN50Len());
 		
-		Assert.assertEquals(198559, n50.getAllContigsLen());
-//		Trinity计算出来的N50结果为5519,我们计算出来的是5460;	
-		Assert.assertEquals(5519, n50.getN50Len());
-		Assert.assertEquals(5460, n50.getN50Len());
-
+		String chrFile2 = "/home/novelbio/git/NBCplatform/src/test/resources/test_file/denovo/Trinity.fasta.gz";
+		N50statistics n50stat = new N50statistics(chrFile2);
+		n50stat.doStatistics();
+		n50stat.getLsNinfo();
+		Assert.assertEquals(198559, n50stat.getAllContigsLen());
+		Assert.assertEquals(5519, n50stat.getN50Len());
+		
 		try{
 //				n50.getN50Len(101);
 	    }catch(Exception ex){
