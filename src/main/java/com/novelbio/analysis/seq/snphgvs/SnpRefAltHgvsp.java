4a9d49b2b50d6aa9a6196cd090b8d471fae90323
zong0jie <zongjie@novelbio.com>
2018-01-16 12:02:56 +0800

    1. 继续写Hgvsp，现在写Deletion的蛋白注释

--- src/main/java/com/novelbio/analysis/seq/snphgvs/SnpRefAltHgvsp.java
+++ src/main/java/com/novelbio/analysis/seq/snphgvs/SnpRefAltHgvsp.java
@@ -10,6 +10,8 @@ import com.novelbio.analysis.seq.genome.gffOperate.ExonInfo;
 import com.novelbio.analysis.seq.genome.gffOperate.GffGeneIsoInfo;
 import com.novelbio.base.dataStructure.ArrayOperate;
 
+import smile.math.Math;
+
 public abstract class SnpRefAltHgvsp {
 	SnpRefAltHgvsc snpRefAltIso;
 	/** 是否需要蛋白注释 */
@@ -35,9 +37,14 @@ public abstract class SnpRefAltHgvsp {
 	public int getStartCis() {
 		return iso.isCis5to3() ? snpRefAltInfo.getStartReal() : snpRefAltInfo.getEndReal();
 	}
-	
 	public int getEndCis() {
 		return iso.isCis5to3() ? snpRefAltInfo.getEndReal() : snpRefAltInfo.getStartReal();
+	}
+	public int getStartAbs() {
+		return snpRefAltInfo.getStartReal();
+	}
+	public int getEndAbs() {
+		return snpRefAltInfo.getEndReal();
 	}
 	
 	/** 是否需要氨基酸变化注释，有些在内含子中的就不需要氨基酸变化注释 */
@@ -133,10 +140,10 @@ class SnpRefAltIsoSnp extends SnpRefAltHgvsp {
 
 }
 
-class SnpRefAltIsoInsert extends SnpRefAltHgvsp {
+class SnpRefAltIsoIns extends SnpRefAltHgvsp {
 	boolean isInsertInFrame = false;
 	
-	public SnpRefAltIsoInsert(SnpRefAltInfo snpRefAltInfo, GffGeneIsoInfo iso) {
+	public SnpRefAltIsoIns(SnpRefAltInfo snpRefAltInfo, GffGeneIsoInfo iso) {
 		super(snpRefAltInfo, iso);
 	}
 	
@@ -162,6 +169,181 @@ class SnpRefAltIsoInsert extends SnpRefAltHgvsp {
 			startCds = iso.getLsElement().get(Math.abs(startNum)-1).getEndCis();
 		}
 		int endNum = iso.getNumCodInEle(endCds);
+		if (endNum < 0) {
+			endCds = iso.getLsElement().get(Math.abs(endNum)).getStartCis();
+		}
+		
+		if (iso.getCod2ATGmRNA(startCds)%3 == 0) {
+			isInsertInFrame = true;
+		}
+		
+		//如果在两个密码子中插入碱基，类似
+		// AT[G] -ACT- AGC，其中start为G
+		//则我们获取 [A]TG-ACT-AG[C] 
+		//最后突变类型变为 ATG_AGCinsACT
+		if (isInsertInFrame && !isFrameShift()) {
+			startCds = iso.getLocAAbefore(startCds);
+			endCds = iso.getLocAAendBias(endCds);
+			return;
+		}
+		
+		//如果在两个密码子中插入造成移码突变，类似
+		// ATG-A-ACT AGC
+		//则我们获取 ATG-A-[ACT AGC ....] 一直到结束
+		//最后突变类型变为 [ACT] > [-A-AC] Ter * 其中*是数字
+		if (isInsertInFrame) {
+			startCds = endCds;
+			endCds = iso.getEnd();
+			return;
+		}
+		
+		//如果在读码框内插入造成移码突变，类似
+		// A[T]-A-G ACT AGC，其中start为T
+		//则我们获取 [A]T-A-G ACT AGC .... 一直到结束
+		//最后突变类型变为 [ACG] > [AT-A-] Ter * 其中*是数字
+		if (!isInsertInFrame()) {
+			startCds = iso.getLocAAbefore(startCds);
+			endCds = iso.getEnd();
+		}
+		endCds = iso.getLocAAend(startCds);
+		return;
+	}
+	
+	protected void setSiteReplace() {
+		int startNum = iso.getNumCodInEle(getStartCis());
+		if (startNum < 0) {
+			startCds = iso.getLsElement().get(Math.abs(startNum)).getEndAbs();
+		}
+	
+		if (isInsertInFrame && isFrameShift()) {
+			// 如果在两个密码子中插入造成移码突变，类似
+			// ATG-A-ACT AGC
+			// 则我们获取 ATG-A-[ACT AGC ....] 一直到结束
+			// 最后突变类型变为 [ACT] > [-A-AC] Ter * 其中*是数字
+			// 因此就只需要简单的将插入序列添加到ref序列的头部即可
+			snpOnReplaceLocStart = 0;
+			snpOnReplaceLocEnd = 0;
+		} else {
+			snpOnReplaceLocStart = -iso.getLocAAbeforeBias(startCds) + 1;
+			snpOnReplaceLocEnd = snpOnReplaceLocStart;
+		}
+	}
+	
+	public String getSnpChange() {
+		boolean isFrameShift = snpRefAltInfo.getSeqAlt().length()%3 != 0;
+		String info = isFrameShift ? getInsertionChangeInFrame() : getInsertionChangeFrameShift();
+		return "p." + info;
+	}
+	
+	/** 读码框内的插入改变 */
+	private String getInsertionChangeInFrame() {
+		char[] refAA = refSeqNrForAA.toStringAA1().toCharArray();
+		//应该都是2
+		if (refAA.length != 2) {
+			throw new ExceptionNBCSnpHgvs("error");
+		}
+		char[] altAA = altSeqNrForAA.toStringAA1().toCharArray();
+		if (altAA[0] != refAA[0] || altAA[altAA.length-1] != refAA[1]) {
+			throw new ExceptionNBCSnpHgvs("error");
+		}
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append(refAA[0] + getAffectAANum(startCds) + "_" + refAA[1] + getAffectAANum(endCds) + "ins");	
+		for (int i = 1; i < altAA.length-1; i++) {
+			sBuilder.append(altAA[i]);	
+		}
+		return sBuilder.toString();
+	}
+
+	/** 读码框外的插入改变 */
+	private String getInsertionChangeFrameShift() {
+		String refSeq = refSeqNrForAA.toStringAA1().substring(0,1);
+		String aaSeq = altSeqNrForAA.toStringAA1();
+		int terNum = 0;
+		boolean isHaveTer = false;
+		char[] aaSeqChr = aaSeq.toCharArray();
+		for (char aaChar : aaSeq.toCharArray()) {
+			if (aaChar == '*') {
+				isHaveTer = true;
+				break;
+			}
+			terNum++;
+		}
+		String ter = isHaveTer? terNum+"" : "?";
+		return refSeq + getAffectAANum(startCds) + aaSeqChr[0] + "fsTer" + ter;
+	}
+
+	
+	/** 插入位置是否在两个aa中间 */
+	private boolean isInsertInFrame() {
+		return iso.getCod2ATGmRNA(startCds)%3 == 0;
+	}
+	
+	/** 插入是否引起了移码 */
+	private boolean isFrameShift() {
+		return snpRefAltInfo.getSeqAlt().length()%3 != 0;
+	}
+	
+}
+
+
+class SnpRefAltIsoDel extends SnpRefAltHgvsp {
+	boolean isDelInFrame = false;
+	boolean isNeedAAanno = false;
+	
+	/** 有这个标记的直接返回Met1?或者Met1fs */
+	boolean isMetDel = false;
+	
+	public SnpRefAltIsoDel(SnpRefAltInfo snpRefAltInfo, GffGeneIsoInfo iso) {
+		super(snpRefAltInfo, iso);
+		
+		int[] region = getValidRange(new int[]{getStartCis(), getEndCis()}, new int[]{iso.getATGsite(), iso.getUAGsite()});
+		if(region == null) return;
+		List<ExonInfo> lsExons = iso.getRangeIsoOnExon(region[0], region[1]);
+		isNeedAAanno = !lsExons.isEmpty();
+		
+		int totalLength = lsExons.stream()
+				.map(it -> it.getLength())
+				.reduce(0, (result, element) -> result + element);
+		isDelInFrame = totalLength % 3 == 0;
+	}
+	
+	public boolean isNeedAAanno() {
+		return isNeedAAanno;
+	}
+	
+	private int[] getValidRange(int[] startEnd, int[] atgUag) {
+		int[] startend = new int[] {Math.min(startEnd[0], startEnd[1]), Math.max(startEnd[0], startEnd[1])};
+		int[] atguag = new int[] {Math.min(atgUag[0], atgUag[1]), Math.max(atgUag[0], atgUag[1])};
+		int[] result = new int[] {Math.max(startend[0], atguag[0]), Math.min(startend[1], atguag[1])};
+		if(result[1] < result[0]) {
+			return null;
+		}
+		return result;
+	}
+	
+	protected void setStartEndCis() {
+		int atgStart = iso.getATGsite();
+		int atgEnd = iso.getLocAAend(iso.getATGsite());
+		int atgBefore = Math.min(atgStart, atgEnd);
+		int atgAfter = Math.max(atgStart, atgEnd);
+		
+		if (getStartAbs() <= atgBefore && getEndAbs()>= atgBefore) {
+			isMetDel = true;
+			return;
+		}
+		startCds = getStartCis();
+		endCds = getEndCis();
+		
+		
+		int startNum = iso.getNumCodInEle(startCds);
+		int endNum = iso.getNumCodInEle(endCds);
+		//跳过了一个或多个exon
+		if (startNum < 0 && endNum < 0) {
+			startCds = iso.getLsElement().get(Math.abs(startNum)).getStartAbs();
+			endCds = iso.getLsElement().get(Math.abs(endNum)-1).getEndAbs();
+		} else {
+			
+		}
 		if (endNum < 0) {
 			endCds = iso.getLsElement().get(Math.abs(endNum)).getStartCis();
 		}
