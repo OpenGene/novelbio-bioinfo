bfb7b0c2c5852c811c6977dc093c4f3daa6629fa
ZongJie <zongjie@novelbio.com>
2018-05-07 10:10:44 +0800

    1. 上传species相关domain

--- src/main/java/com/novelbio/database/domain/speciesdb/SpeciesUploadFile.java
+++ src/main/java/com/novelbio/database/domain/speciesdb/SpeciesUploadFile.java
@@ -1,6 +1,11 @@
 package com.novelbio.database.domain.speciesdb;
 
+import java.io.InputStream;
+
+import com.novelbio.base.fileOperate.FileOperate;
+import com.novelbio.database.model.geneanno.SpeciesFile;
 import com.novelbio.database.service.species.MgmtSpecies;
+import com.novelbio.generalConf.PathDetailNBC;
 
 /** 上传文件 */
 public class SpeciesUploadFile {
@@ -10,5 +15,10 @@ public class SpeciesUploadFile {
 	public void uploadFile(int taxId, String version, String fileType) {
 		
 	}
-	
+	public void uploadChrFile(int taxId, String version, InputStream is) {
+		String basePath = PathDetailNBC.getGenomePath() + SpeciesFile.SPECIES_FOLDER + FileOperate.getSepPath()
+		+ taxId + FileOperate.getSepPath() + version + FileOperate.getSepPath();
+		String resultFile = basePath + "ChromFa/chrAll.fa";
+		
+	}
 }
